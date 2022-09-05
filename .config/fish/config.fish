a6033a9d8d4dc4af72264f4a353b3377f80e05dd
Dan Mezhiborsky <54985569+dmezh@users.noreply.github.com>
2022-09-05 02:53:50 -0400

    Add -h to ls aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,9 +3,9 @@ set -U fish_user_paths /opt/homebrew/bin $fish_user_paths
 alias gst="git status"
 alias vim="nvim"
 alias ls="ls --color"
-alias ll="ls -l --color"
-alias la="ls -a --color"
-alias lla="ls -la --color"
+alias ll="ls -lh --color"
+alias la="ls -ah --color"
+alias lla="ls -lah --color"
 
 thefuck --alias | source
 
