89f1627cfe79da1b88d7c574bb2013e68b040160
Dan Mezhiborsky <54985569+dmezh@users.noreply.github.com>
2022-10-27 20:31:24 -0400

    config.fish updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,12 @@
 fish_add_path /opt/homebrew/bin
 fish_add_path /opt/homebrew/opt/binutils/bin
+fish_add_path ~/.cargo/bin
+fish_add_path /opt/homebrew/opt/llvm@14/bin
 
 alias gst="git status"
+alias g="git"
+alias gsw="git switch"
+alias gch="git checkout"
 alias vim="nvim"
 alias ls="ls --color"
 alias ll="ls -lh --color"
